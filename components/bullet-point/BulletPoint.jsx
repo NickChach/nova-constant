8ba6611f575f85a0bd15b345df071924e29a7be7
NickChach <debian5user@gmail.com>
2026-05-19 17:55:08 +0300

    images preloaded instead of priority

--- components/bullet-point/BulletPoint.jsx
+++ components/bullet-point/BulletPoint.jsx
@@ -1,9 +1,9 @@
 import styles from "./bullet-point.module.css";
 import { LuArrowBigRight } from "react-icons/lu";
 
-export default function BulletPoint({ key, title, content }) {
+export default function BulletPoint({ title, content }) {
 	return (
-		<section key={key} className={styles.bulletPoint}>
+		<section className={styles.bulletPoint}>
 			<h2>{title}</h2>
 			<LuArrowBigRight className={styles.icon} />
 			<p>{content}</p>
